b729f1a12644d33682f4ea99c9c1702fa9c8081b
JaydenWork <kkangg92@gmail.com>
2023-05-15 20:10:15 +0900

    메인페이지 인터셉터로 실행 버전+admin 로그인 창

--- src/main/java/edu/nojo/vote/administrator/controller/AdministratorController.java
+++ src/main/java/edu/nojo/vote/administrator/controller/AdministratorController.java
@@ -1,5 +1,13 @@
 package edu.nojo.vote.administrator.controller;
 
-public class AdministratorController {
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
+@Controller
+public class AdministratorController {
+	
+	@GetMapping("/adminLogin")
+	public String adminLogin() {
+		return "admin/adminLogin";
+	}
 }

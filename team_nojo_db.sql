d9c612ee4f6ff17b9ea842253355cfd2a1783346
DESKTOP-IJ8C807\user1 <kobcom12@naver.com>
2023-05-16 14:50:49 +0900

    230516 db 규원씨가 작성해주신 샘플 데이터 추가, sql에 잘못 적혀있던 로그인

--- team_nojo_db.sql
+++ team_nojo_db.sql
@@ -48,18 +48,6 @@ VALUES (SEQ_USER_NO.NEXTVAL, 1, '/resources/images/mainPage/profile7.jpeg', 'use
 		'유저칠번', 'pass07!', '서울시 종로 율곡로 2', '유저 칠 번 입니다.', NULL, DEFAULT, DEFAULT);
 	
 	
-SELECT * FROM "USER";	
-
--- 로그인
-SELECT USER_NO, USER_EMAIL, USER_GRADE, USER_NICKNAME, USER_ADD, USER_IMG, USER_PW, 
-				TO_CHAR(USER_ENDROLL_DT, 'YYYY"년" MM"월" DD"일" HH24"시" MI"분" SS"초"') USER_ENDROLL_DT
-		FROM "USER"
-		WHERE USER_DEL_FL = 'N'
-		AND USER_EMAIL = 'user03@kh.or.kr';
-
--- 비밀번호 암호화
-UPDATE "USER" SET
-USER_PW = '$2a$10$nxEfdIGsSboDg.32irKM/uj3AwlaQqpFasc0T.xSAW2JkTIdO7Uhe';
 
 COMMIT;
 	
@@ -90,41 +78,7 @@ COMMENT ON COLUMN "USER"."USER_DEL_FL" IS '회원 탈퇴여부(Y:탈퇴/N:탈퇴
 
 
 
-INSERT INTO "PETITION"
-VALUES (SEQ_USER_NO.NEXTVAL, '尹, 중·러 겨냥…“힘에 의한 현상 변경, 꿈꿀 수 없게 해야"', 
-'윤석열 대통령은 28일(현지시간) 보스턴 하버드 대학 케네디스쿨에서 연설하면서 다른 나라의 자유를 무시하는 힘에 의한 현상 변경 시도’에 국제사회는 용기 있고 결연한 연대로서 대응해야 한다고 밝혔다. 하버드대에서 현직 대통령이 연설하는 것은 처음이다. 윤 대통령은...', 
-TO_DATE('2022-05-10', 'YYYY-MM-DD'), 152, 'N', 'resources/images/mainPage/news1.jpeg', 'N', 3, 1);
 
-INSERT INTO "PETITION"
-VALUES (SEQ_USER_NO.NEXTVAL, '손흥민, 1골 추가하면 호날두와 동률 … 7년 연속 리그 10호 골 안필드에서 터질까', 
-'이번에는 안필드다. 지난 주 맨유를 상대로 극적인 동점골을 터트리며 팀을 위기에서 구해낸 손흥민이 5월 1일 월요일(한국시간) 새벽 0시 30분 리버풀 원정에서 2경기 연속 골에 도전한다.올 시즌 리그 9호 골을 기록한 손흥민은 최근 리그 10경기에서 5골을 넣으며 완연한 회복세를 보이...', 
-TO_DATE('2022-05-29', 'YYYY-MM-DD'), 32352, 'N', 'resources/images/mainPage/news2.jpeg', 'N', 2, 2);
-
-INSERT INTO "PETITION"
-VALUES (SEQ_USER_NO.NEXTVAL, '"나폴리 우승, 이번주 안돼".. 라치오 감독도 단장도 한목소리 다짐', 
-'이 경기는 나폴리의 우승과 직결된다는 점에서 많은 팬들의 관심이 쏠려 있다. 2위 라치오가 이날 경기에서 이기지 못할 경우 나폴리의 세리에 A 우승이 눈앞에 다가서기 때문이다. 나폴리가 인테르-라치오전에 이어 열리는 살레르니타나와 맞대결에서 승리할 경우 33년 만의 우승을 확정짓게 된다. ',
-TO_DATE('2021-09-11', 'YYYY-MM-DD'), 5678, 'N', 'resources/images/mainPage/news3.jpeg', 'N', 1, 1);
-
-INSERT INTO "PETITION"
-VALUES (SEQ_USER_NO.NEXTVAL, '[단독]현대차, 아이오닉6 N 개발 착수…"고성능 전기차로 中 승부수"', 
-'아이오닉6 N은 제품 개발을 거쳐 2025년 하반기부터 현대차 아산공장에서 내수와 유럽·호주 수출 물량을 우선 생산한다. 2026년 초부터는 북미·중국 물량을 추가로 생산한다. 호주 등을 위해 좌핸들 차량 외에 우핸들 차량도 선보인다.주력 시장은 중국이다. 현대차는 ',
-TO_DATE('2023-01-22', 'YYYY-MM-DD'), 31528, 'N', 'resources/images/mainPage/news4.jpeg', 'N', 4, 2);
-
-INSERT INTO "PETITION"
-VALUES (SEQ_USER_NO.NEXTVAL, '바이든, ‘요즘 신나’…“1조원 날린 폭스 뉴스, 공짜밥 먹으러” 농담',
- '조 바이든 미국 대통령이 백악관 출입기자들과의 연례 만찬 행사에서 손해배상 소송에 대해 7억8750만달러(약 1조560억원)를 합의금으로 물기로 한 <폭스 뉴스>를 마음껏 놀렸다.바이든 대통령은 29일 저녁(현지시각) 백악관 출입기자협회 연례',
-TO_DATE('2023-03-23', 'YYYY-MM-DD'), 1911, 'N', 'resources/images/mainPage/news5.jpeg', 'N', 5, 3);
-
-INSERT INTO "PETITION"
-VALUES (SEQ_USER_NO.NEXTVAL, '주가 상승엔 성장과 실적 확신이 필요하다',
- '최근 국내 증시가 불안한 흐름을 보이고 있다. 이달 중순까지 910포인트에 근접하며 빠르게 오르던 코스닥 지수는 며칠 새 10% 가까이 급락했고, 코스피 역시 5거래일 연속 하락하며 위축된 투자심리를 보여주고 있다. 같은 기간 글로벌 증시 역시 상승세가 꺾였기 때문에 증시 부진을 우리만의 일이라 볼 순 없다.',
-TO_DATE('2023-02-11', 'YYYY-MM-DD'), 88852, 'N', 'resources/images/mainPage/news6.jpeg', 'N', 6, 1);
-
-COMMIT;
-
-
-
-SELECT * FROM "PETITION";
 
 
 -- FAQ 테이블 생성
@@ -189,7 +143,6 @@ COMMENT ON COLUMN "COMMENT"."CM_DEL_FL" IS '댓글 삭제(삭제:Y, 유지:N)';
 
 
 
-
 -- PETITION 테이블 생성
 CREATE TABLE "PETITION" (
 	"PT_NO"	NUMBER		NOT NULL,
@@ -229,10 +182,107 @@ COMMENT ON COLUMN "PETITION"."USER_NO" IS '회원번호(SEQ_USER_NO)';
 
 COMMENT ON COLUMN "PETITION"."CAT_NO" IS '카테고리 코드(SEQ_CAT_NO)';
 
-/*RENAME "PETITION" TO "PETITION"*/
+
+--PETITION 샘플데이터
+INSERT INTO "PETITION"
+VALUES (SEQ_USER_NO.NEXTVAL, '尹, 중·러 겨냥…“힘에 의한 현상 변경, 꿈꿀 수 없게 해야"', 
+'윤석열 대통령은 28일(현지시간) 보스턴 하버드 대학 케네디스쿨에서 연설하면서 다른 나라의 자유를 무시하는 힘에 의한 현상 변경 시도’에 국제사회는 용기 있고 결연한 연대로서 대응해야 한다고 밝혔다. 하버드대에서 현직 대통령이 연설하는 것은 처음이다. 윤 대통령은...', 
+TO_DATE('2022-05-10', 'YYYY-MM-DD'), 152, 'N', 'resources/images/mainPage/news1.jpeg', 'N', 3, 1);
+
+INSERT INTO "PETITION"
+VALUES (SEQ_USER_NO.NEXTVAL, '손흥민, 1골 추가하면 호날두와 동률 … 7년 연속 리그 10호 골 안필드에서 터질까', 
+'이번에는 안필드다. 지난 주 맨유를 상대로 극적인 동점골을 터트리며 팀을 위기에서 구해낸 손흥민이 5월 1일 월요일(한국시간) 새벽 0시 30분 리버풀 원정에서 2경기 연속 골에 도전한다.올 시즌 리그 9호 골을 기록한 손흥민은 최근 리그 10경기에서 5골을 넣으며 완연한 회복세를 보이...', 
+TO_DATE('2022-05-29', 'YYYY-MM-DD'), 32352, 'N', 'resources/images/mainPage/news2.jpeg', 'N', 2, 2);
+
+INSERT INTO "PETITION"
+VALUES (SEQ_USER_NO.NEXTVAL, '"나폴리 우승, 이번주 안돼".. 라치오 감독도 단장도 한목소리 다짐', 
+'이 경기는 나폴리의 우승과 직결된다는 점에서 많은 팬들의 관심이 쏠려 있다. 2위 라치오가 이날 경기에서 이기지 못할 경우 나폴리의 세리에 A 우승이 눈앞에 다가서기 때문이다. 나폴리가 인테르-라치오전에 이어 열리는 살레르니타나와 맞대결에서 승리할 경우 33년 만의 우승을 확정짓게 된다. ',
+TO_DATE('2021-09-11', 'YYYY-MM-DD'), 5678, 'N', 'resources/images/mainPage/news3.jpeg', 'N', 1, 1);
+
+INSERT INTO "PETITION"
+VALUES (SEQ_USER_NO.NEXTVAL, '[단독]현대차, 아이오닉6 N 개발 착수…"고성능 전기차로 中 승부수"', 
+'아이오닉6 N은 제품 개발을 거쳐 2025년 하반기부터 현대차 아산공장에서 내수와 유럽·호주 수출 물량을 우선 생산한다. 2026년 초부터는 북미·중국 물량을 추가로 생산한다. 호주 등을 위해 좌핸들 차량 외에 우핸들 차량도 선보인다.주력 시장은 중국이다. 현대차는 ',
+TO_DATE('2023-01-22', 'YYYY-MM-DD'), 31528, 'N', 'resources/images/mainPage/news4.jpeg', 'N', 4, 2);
+
+INSERT INTO "PETITION"
+VALUES (SEQ_USER_NO.NEXTVAL, '바이든, ‘요즘 신나’…“1조원 날린 폭스 뉴스, 공짜밥 먹으러” 농담',
+ '조 바이든 미국 대통령이 백악관 출입기자들과의 연례 만찬 행사에서 손해배상 소송에 대해 7억8750만달러(약 1조560억원)를 합의금으로 물기로 한 <폭스 뉴스>를 마음껏 놀렸다.바이든 대통령은 29일 저녁(현지시각) 백악관 출입기자협회 연례',
+TO_DATE('2023-03-23', 'YYYY-MM-DD'), 1911, 'N', 'resources/images/mainPage/news5.jpeg', 'N', 5, 3);
+
+INSERT INTO "PETITION"
+VALUES (SEQ_USER_NO.NEXTVAL, '주가 상승엔 성장과 실적 확신이 필요하다',
+ '최근 국내 증시가 불안한 흐름을 보이고 있다. 이달 중순까지 910포인트에 근접하며 빠르게 오르던 코스닥 지수는 며칠 새 10% 가까이 급락했고, 코스피 역시 5거래일 연속 하락하며 위축된 투자심리를 보여주고 있다. 같은 기간 글로벌 증시 역시 상승세가 꺾였기 때문에 증시 부진을 우리만의 일이라 볼 순 없다.',
+TO_DATE('2023-02-11', 'YYYY-MM-DD'), 88852, 'N', 'resources/images/mainPage/news6.jpeg', 'N', 6, 1);
 
 
+--7
+INSERT INTO "PETITION"
+VALUES (SEQ_USER_NO.NEXTVAL, '15억 손해 보고 바꿨다… 김남국, 작년 2월 ‘이상한 코인 교환’',
+ '김남국 의원의 ‘위믹스 코인 60억원 보유’ 논란을 수사 중인 검찰이 15일 가상 화폐 거래소인 업비트와 빗썸, 카카오 계열사 등에 대한 압수 수색에 나섰다. 김 의원이 코인 논란으로 지난 14일 더불어민주당을 탈당한 지 하루 만에 김 의원의 가상 화폐 거래 내역 확보에 나선 것이다. 앞서 작년 10~11월 검찰은 김 의원이 보유하고 ',
+TO_DATE('2023-03-11', 'YYYY-MM-DD'), 666234, 'N', 'resources/images/mainPage/news7.jpeg', 'N', 2, 2);
 
+--8
+INSERT INTO "PETITION"
+VALUES (SEQ_USER_NO.NEXTVAL, '테슬라 돌아온 머스크… “모든 채용 계획 보고하라”',
+ 'CNBC는 15일(현지시각) 일론 머스크 테슬라 최고경영자(CEO)가 테슬라 직원 전체에 메일을 보내 “일주일에 한 번 부서 채용 요청 목록을 보내달라”고 요청했다고 보도했다. 그는 메일에서 “우리 채용에 대해 더 잘 이해하고 싶다”고 했다.머스크는 이 메일에서 “(나에게) ',
+TO_DATE('2023-04-11', 'YYYY-MM-DD'), 356457, 'N', 'resources/images/mainPage/news8.jpeg', 'N', 2, 3);
+
+
+--9
+INSERT INTO "PETITION"
+VALUES (SEQ_USER_NO.NEXTVAL, '"마약 5종 투약 혐의" 유아인, 2차 소환 조사…미대 출신 작가도 출석 [MD이슈](종합)',
+ '유아인은 16일 오전 9시께 서울 마포구 서울경찰청 광역수사단에 도착해 기자로부터 심경 질문을 받고 "성실히 조사에 임하겠다"고 답했다.당초 유아인은 지난 11일 2차 소환 조사에 임할 예정이었으나 청사 앞에 대기하는 취재진이 많다는 이유로 조사를 돌연 취소한 것으로 알려졌다. ',
+TO_DATE('2023-05-11', 'YYYY-MM-DD'), 12357, 'N', 'resources/images/mainPage/news9.jpeg', 'N', 3, 2);
+
+--10
+INSERT INTO "PETITION"
+VALUES (SEQ_USER_NO.NEXTVAL, '"인어공주" 더빙판 에리얼=뉴진스 다니엘 “여전히 배울 게 많아” [화보]',
+ '최근 영화 ‘인어공주’ 한국어 더빙판의 주인공 ‘에리얼’ 역할을 맡은 다니엘. 그는 “부모님께서 어려서부터 음악에 관련된 다양한 경험을 시켜주셨다”며 “여전히 배울 게 많다고 느껴서 여러 무대도 찾아보고 선배님들의 노래도 들으면서 제가 할 수 있는 걸 끊임없이 탐구 중”이라고 말했다. ',
+TO_DATE('2023-06-11', 'YYYY-MM-DD'), 78564, 'N', 'resources/images/mainPage/news10.jpeg', 'N', 4,1);
+
+--11
+INSERT INTO "PETITION"
+VALUES (SEQ_USER_NO.NEXTVAL, '"패션 관심 많은 김건희" WP 쓴 단어 뭐길래…"무례" 美독자 발끈',
+ '12일(현지시간) WP는 독자 의견 기고란에 "한국 대통령 부인을 모욕하지 말라"(Please don’t insult South Korea’s first lady)는 제목의 글을 게재했다.버지니아주 폴스처치에 사는 독자 로널드 하이탈라가 보낸 해당 글에서 필자는 지난달 27일 WP 기사에서 김 여사에 대해 "clotheshorse"라고 표현한 것을 지적하면서 이 같은 부정적 논평은 무례하다고 밝혔다.',
+TO_DATE('2013-02-11', 'YYYY-MM-DD'), 23453, 'N', 'resources/images/mainPage/news11.jpeg', 'N', 5, 3);
+
+--12
+INSERT INTO "PETITION"
+VALUES (SEQ_USER_NO.NEXTVAL, 'LG유플러스, 1분기 영업익 2602억…전년 동기 대비 0.4% 하락',
+ 'LG유플러스는 연결 기준 올해 1분기 영업이익이 2602억원으로 지난해 동기보다 0.4% 감소한 것으로 잠정 집계됐다고 11일 공시했다.영업수익(매출)은 3조5413억원으로 작년 동기 대비 3.9% 증가했다. 영업수익에서 단말 수익을 제외한 서비스수익은 2조8243억원으로 1.8% 상승했다. 순이익은 1551억원으로 9% 줄었다. ',
+TO_DATE('2003-02-11', 'YYYY-MM-DD'), 7945, 'N', 'resources/images/mainPage/news12.jpeg', 'N', 6, 2);
+
+--13
+INSERT INTO "PETITION"
+VALUES (SEQ_USER_NO.NEXTVAL, '푸틴 "유물" 탱크 한대 사열…우크라 "러 탱크, 우리 노획한 게 더 많아" 조롱',
+ '러시아 모스크바 붉은광장에서 9일(현지시간) 전승절 기념식이 열린 가운데 어느 때보다 축소돼 운영된 전차 열병식에 이목이 쏠린다.전승절 기념식은 제2차 세계 대전에서 나치 독일의 항복을 받아내 승리한 전승일을 기념하는 행사로 러시아는 이날을 국경일로 지정해 매년 대대적으로 기념해왔다. 특히 이날은 러시아에서 새롭게 개발한 군사장비들을 전세계에 과시하는 날이기도 했다.',
+TO_DATE('2013-02-11', 'YYYY-MM-DD'), 3423, 'N', 'resources/images/mainPage/news13.jpeg', 'N', 1, 1);
+
+--14
+INSERT INTO "PETITION"
+VALUES (SEQ_USER_NO.NEXTVAL, '美정보수장 "中, 대만 침공→세계 연간 1300조원 피해"',
+ '중국의 대만 침공으로 대만의 반도체 수출이 중단된다면 세계 경제가 연간 최대 1천300조원의 피해를 입을 수 있다고 미국 정보수장이 밝혔다.
+애브릴 헤인스 미 국가정보국(DNI) 국장은 4일(현지시간) 상원 군사위원회 청문회에서 "중국 또한 반도체를 구하지 못하면 중국 국내총생산(GDP)에도 큰 악영향이 있을 것"이라며 이같이 밝혔다.헤인스 국장은 "대만이 첨단 반도체의 90% 이상을 생산하는데 그 반도체는 전 세계의 거의 모든 종류의 전자기기에 들어간다"며 "중국이 대만을 침공할 경우 TSMC의 반도체 생산이 중단되',
+TO_DATE('2023-12-11', 'YYYY-MM-DD'), 12345, 'N', 'resources/images/mainPage/news14.jpeg', 'N', 2, 3);
+
+--15
+INSERT INTO "PETITION"
+VALUES (SEQ_USER_NO.NEXTVAL, '英석학 "후쿠시마 오염수, 마셔도 돼"…"당장 마셔주세요"',
+ '정부의 후쿠시마 오염수 시찰단 일본 방문이 약 1주일 앞둔 가운데 해외 석학이 오염수의 위험성이 과장됐다며 "마실 수 있다"고 발언해 논란이 일고 있다.방사선 분야에서 세계적인 석학으로 꼽히는 웨이드 앨리슨 영국 옥스퍼드대 물리학과 명예교수(82)는 15일 한국원자력연구원과 한국원자력학회가 "저선량 방사선 영향과 후쿠시마 오염수 논란-공포가 집어삼킨 과학"을 주제로 연 기자간담회에서',
+TO_DATE('2023-02-20', 'YYYY-MM-DD'), 3256, 'N', 'resources/images/mainPage/news15.jpeg', 'N', 3, 2);
+
+--16
+INSERT INTO "PETITION"
+VALUES (SEQ_USER_NO.NEXTVAL, '여고생 노렸다…필로폰 중독시켜 판매 동원한 마약판매상들',
+ '대구지방검찰청은 대구 주요 마약 판매상 등 8명을 구속 기소하고 마약 투여자 1명을 불구속 기소했다고 16일 밝혔다. 검찰은 마약 판매상의 권유로 마약에 손을 댄 여고생에 대해서는 중독 판별 검사를 통한 치료 가능 여부 등을 확인하기 위해 시한부 기소 중지 결정을 내렸다.검찰에 따르면 A(18)양은 고등학생이던 지난 2020년 알',
+TO_DATE('2023-02-11', 'YYYY-MM-DD'), 123123, 'N', 'resources/images/mainPage/news16.jpe', 'N', 2, 2);
+
+
+COMMIT;
+
+DELETE FROM "PETITION" WHERE PT_NO = 63;
+
+SELECT * FROM "PETITION";
 
 
 

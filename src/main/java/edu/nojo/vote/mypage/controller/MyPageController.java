830a64f855940e7d1180f0e850505510ee5949c5
DESKTOP-IJ8C807\user1 <kobcom12@naver.com>
2023-05-18 16:58:01 +0900

    230518 mypage 패키지명 오류 수정

--- src/main/java/edu/nojo/vote/mypage/controller/MyPageController.java
+++ src/main/java/edu/nojo/vote/mypage/controller/MyPageController.java
@@ -2,11 +2,7 @@ package edu.nojo.vote.mypage.controller;
 
 import java.io.IOException;
 
-import javax.servlet.http.HttpSession;
-
-import org.apache.ibatis.javassist.compiler.ast.Member;
 import org.springframework.beans.factory.annotation.Autowired;
-
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -18,7 +14,6 @@ import org.springframework.web.bind.annotation.SessionAttributes;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
-
 import edu.nojo.vote.mypage.model.service.MyPageService;
 import edu.nojo.vote.user.model.dto.User;
 
@@ -27,62 +22,62 @@ import edu.nojo.vote.user.model.dto.User;
 @RequestMapping("/myPage")
 @Controller
 public class MyPageController {
-	
-	@Autowired
-	private MyPageService service;
-	
-	
-	
-	// editProfil로 이동
-	@GetMapping("/editProfile")
-	public String myPetitionDetails() {
-		return "/myPage/editProfile";
-	}
-	
-	
-	
-	// 닉네임 중복 검사
-	@GetMapping("/dupCheck/nickname")
-	@ResponseBody
-	public int checkNickname(@RequestParam String nickname) {
-		
-		//System.out.println(service.checkNickname(nickname));
-		return service.checkNickname(nickname);
-	}
-	
-	
-	
-	// 프로필 이미지 수정
-	@PostMapping("/editProfile")
-	public String updateProfile(
-			@RequestParam("userImage") MultipartFile userImage // 업로드 파일
-			, @SessionAttribute("loginUser") User loginUser // 로그인 회원
-			, RedirectAttributes ra // 리다이렉트 시 메세지 전달
-//			, HttpSession session // 세션 객체
-			)   throws IllegalStateException, IOException{
-		
-		
-			// 웹 접근 경로
-			String Path = "/resources/images/user/" + userImage.getOriginalFilename();
-			
-			// 프로필 이미지 수정 서비스 호출
-			int result = service.updateProfile(userImage, Path, loginUser);
-			
-			String message = null;
-			if(result > 0)	message = "프로필 이미지가 변경";
-			else			message = "프로필 이미지 변경 실패";
-			
-			ra.addFlashAttribute("message", message);
-			
-			return "redirect:editProfile";
-	}
-	
-	
-	
-	
-	
-	
-	
-	
+   
+   @Autowired
+   private MyPageService service;
+   
+   
+   
+   // editProfil로 이동
+   @GetMapping("/editProfile")
+   public String myPetitionDetails() {
+      return "/myPage/editProfile";
+   }
+   
+   
+   
+   // 닉네임 중복 검사
+   @GetMapping("/dupCheck/nickname")
+   @ResponseBody
+   public int checkNickname(@RequestParam String nickname) {
+      
+      //System.out.println(service.checkNickname(nickname));
+      return service.checkNickname(nickname);
+   }
+   
+   
+   
+   // 프로필 이미지 수정
+   @PostMapping("/editProfile")
+   public String updateProfile(
+         @RequestParam("userImage") MultipartFile userImage // 업로드 파일
+         , @SessionAttribute("loginUser") User loginUser // 로그인 회원
+         , RedirectAttributes ra // 리다이렉트 시 메세지 전달
+//         , HttpSession session // 세션 객체
+         )   throws IllegalStateException, IOException{
+      
+      
+         // 웹 접근 경로
+         String Path = "/resources/images/user/" + userImage.getOriginalFilename();
+         
+         // 프로필 이미지 수정 서비스 호출
+         int result = service.updateProfile(userImage, Path, loginUser);
+         
+         String message = null;
+         if(result > 0)   message = "프로필 이미지가 변경";
+         else         message = "프로필 이미지 변경 실패";
+         
+         ra.addFlashAttribute("message", message);
+         
+         return "redirect:editProfile";
+   }
+   
+   
+   
+   
+   
+   
+   
+   
 
 }
